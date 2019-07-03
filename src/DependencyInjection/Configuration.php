4973521efd1de833cefb401b7abc69ec49bcbaa3
Jonathan Wilkinson <jonny@ctrl-hub.com>
2019-07-03 15:37:52 +0100

    Update TreeBuilder instantiation - 4.3 deprecation fix.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,9 +18,9 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('ashley_dawson_class_meta');
 
-        $rootNode = $treeBuilder->root('ashley_dawson_class_meta');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
